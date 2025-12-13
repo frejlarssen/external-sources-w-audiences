1b7439fe2dc30bab6b59cfeb91259eabcce70a1e
Frej Larssen <frejlarssen@hotmail.se>
2025-12-13 20:48:58 +0100

    Generalizing plotting to arbitrary parameters and plotting total sum

--- src/plotting.py
+++ src/plotting.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import re
+from pathlib import Path
 
 def sanitize_label(label):
     label = re.sub(r'mathbf', '', label)
@@ -8,31 +9,31 @@ def sanitize_label(label):
     return re.sub(r'[^a-zA-Z0-9_-]', '', label)
 
 # Comparing up to four functions agains some x_values
-def cmp_funcs(alpha, beta, gamma, delta, k, n, e_top_s, trunc,
+def cmp_funcs(params,
                        x_values, x_label, y_label,
                        T_func1, T_func1_label, T_func2=None, T_func2_label=None, 
                        T_func3=None, T_func3_label=None, T_func4=None, T_func4_label=None,
                        T_func5=None, T_func5_label=None,
                        T_func6=None, T_func6_label=None,
-                       yscale='linear'):
+                       yscale='linear', filename=None):
 
-    y_values1 = T_func1(alpha, beta, gamma, delta, k, n, e_top_s, trunc)
+    y_values1 = T_func1(params)
 
     plt.plot(x_values, y_values1, label=T_func1_label, linestyle='--')
     if (T_func2):
-        y_values2 = T_func2(alpha, beta, gamma, delta, k, n, e_top_s, trunc)
+        y_values2 = T_func2(params)
         plt.plot(x_values, y_values2, label=T_func2_label, linestyle='-.')
     if (T_func3):
-        y_values3 = T_func3(alpha, beta, gamma, delta, k, n, e_top_s, trunc)
+        y_values3 = T_func3(params)
         plt.plot(x_values, y_values3, label=T_func3_label, linestyle=':')
     if (T_func4):
-        y_values4 = T_func4(alpha, beta, gamma, delta, k, n, e_top_s, trunc)
+        y_values4 = T_func4(params)
         plt.plot(x_values, y_values4, label=T_func4_label, linestyle='-')
     if (T_func5):
-        y_values5 = T_func5(alpha, beta, gamma, delta, k, n, e_top_s, trunc)
+        y_values5 = T_func5(params)
         plt.plot(x_values, y_values5, label=T_func5_label, linestyle='-')
     if (T_func6):
-        y_values6 = T_func6(alpha, beta, gamma, delta, k, n, e_top_s, trunc)
+        y_values6 = T_func6(params)
         plt.plot(x_values, y_values6, label=T_func6_label, linestyle='-')
     plt.xlabel(x_label)
     plt.ylabel(y_label)
@@ -40,31 +41,35 @@ def cmp_funcs(alpha, beta, gamma, delta, k, n, e_top_s, trunc,
     plt.grid(True)
     plt.legend()
     #plt.tight_layout()
+    
+    if filename == "auto":
+        (alpha, beta, gamma, delta, d, n, e_top_s) = params
+        filename = f"{sanitize_label(T_func1_label)}"
+        if T_func2:
+            filename += f"_and_{sanitize_label(T_func2_label)}"
+        if T_func3:
+            filename += f"_and_{sanitize_label(T_func3_label)}"
+        if T_func4:
+            filename += f"_and_{sanitize_label(T_func4_label)}"
+        if T_func5:
+            filename += f"_and_{sanitize_label(T_func5_label)}"
+        if T_func6:
+            filename += f"_and_{sanitize_label(T_func6_label)}"
+        filename += f"-vs-{sanitize_label(x_label)}"
 
-    #filename = f"{sanitize_label(T_func1_label)}"
-    #if T_func2:
-    #    filename += f"_and_{sanitize_label(T_func2_label)}"
-    #if T_func3:
-    #    filename += f"_and_{sanitize_label(T_func3_label)}"
-    #if T_func4:
-    #    filename += f"_and_{sanitize_label(T_func4_label)}"
-    #if T_func5:
-    #    filename += f"_and_{sanitize_label(T_func5_label)}"
-    #if T_func6:
-    #    filename += f"_and_{sanitize_label(T_func6_label)}"
-    #filename += f"-vs-{sanitize_label(x_label)}"
-#
-    #if not isinstance(alpha, np.ndarray):
-    #    filename += f"-alpha{alpha}"
-    #if not isinstance(beta, np.ndarray):
-    #    filename += f"-beta{beta}"
-    #if not isinstance(gamma, np.ndarray):
-    #    filename += f"-gamma{gamma}"
-    #if trunc:
-    #    filename += f"-trunc"
-    #else:
-    #    filename += f"-nontrunc"
-    #filename += f"-n{n}-e_top_s{e_top_s}.pdf"
-#
-    #plt.savefig("../continuous_plots/" + filename)
+        if not isinstance(alpha, np.ndarray):
+            filename += f"-alpha{alpha}"
+        if not isinstance(beta, np.ndarray):
+            filename += f"-beta{beta}"
+        if not isinstance(gamma, np.ndarray):
+            filename += f"-gamma{gamma}"
+        filename += f"-n{n}-e_top_s{e_top_s}.pdf"
+    
+    if filename != None:
+        BASE_DIR = Path(__file__).resolve().parent
+        output_dir = BASE_DIR / "continuous_plots"
+        output_dir.mkdir(exist_ok=True)
+        print("saving in:", output_dir / filename)
+        plt.savefig(output_dir / filename)
+
     plt.show()
